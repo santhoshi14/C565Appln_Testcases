42ce5dce95907069733d4f46e009eb9cecb38b82
Santhoshi14 <billapatisanthoshi@gmail.com>
2016-06-12 21:24:11 -0500

    First Commit

---
+++ C565ApplnTestCaseProject/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C565Appln_Testcases;
+
+namespace C565ApplnTestCaseProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string strexpected;
+            string actualresult = "Error in push-stack is full";
+            int number = -1;
+            int topindex = -1;
+            int maxlen = -1;
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            strexpected = obj.push(number, topindex, maxlen);
+            Assert.AreEqual(strexpected, actualresult);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string strexpected;
+            string actualresult;
+            int[] stack_ref = new int[100];
+            int number = 2;
+            int topindex = -1;
+            int maxlen = 99;
+            //stack_ref[++topindex] = number;
+
+            //strexpected = stack_ref[++topindex];
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            strexpected = obj.push(number, topindex, maxlen);
+            actualresult = Convert.ToString(++topindex);
+            Assert.AreEqual(strexpected, actualresult);
+
+
+        }
+        [TestMethod]
+        public void poptestmethod1()
+        {
+            string strexpected;
+            string stractualresult = "Error in push-stack is empty";
+            int top_index = -1;
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            strexpected = obj.pop(top_index);
+            Assert.AreEqual(strexpected, stractualresult);
+
+        }
+
+        [TestMethod]
+        public void poptestmethod2()
+        {
+            string strexpected;
+            string stractualresult;
+            int top_index = 2;
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            strexpected = obj.pop(top_index);
+            stractualresult = Convert.ToString(--top_index);
+            Assert.AreEqual(strexpected, stractualresult);
+
+        }
+        //Null Reference Exception Handling for an Array
+        [TestMethod]
+        public void TestNullReference()
+        {
+            int[] stack_ref = new int[100];
+
+            foreach (var value in stack_ref)
+            {
+                if (stack_ref != null)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestTopMethod()
+        {
+            int actualresult = 10;
+            int expectedresult;
+
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            expectedresult = obj.top();
+            Assert.AreEqual(expectedresult, actualresult);
+
+
+        }
+        [TestMethod]
+        public void TestboolMethod()
+        {
+            bool actualresult = true;
+            bool expectedresult;
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            expectedresult = obj.empty();
+            Assert.AreEqual(expectedresult, actualresult);
+        }
+
+        [TestMethod]
+        public void TestboolMethod2()
+        {
+            bool actualresult = false;
+            bool expectedresult;
+            C565Appln_Testcases.Class1 obj = new C565Appln_Testcases.Class1();
+            expectedresult = obj.empty();
+            Assert.AreNotEqual(expectedresult, actualresult);
+
+        }
+
+    }
+}
--- C565Appln_Testcases/Class1.cs
+++ C565Appln_Testcases/Class1.cs
@@ -8,5 +8,59 @@ namespace C565Appln_Testcases
 {
     public class Class1
     {
+        string myString;
+        private int[] stack_ref = new int[100];
+
+        //private int max_len=99, top_index=-1;
+        //public StackClass()
+        //{ // A constructor
+        //    stack_ref = new int[100];
+        //    max_len = 99;
+        //    top_index = -1;
+        //}
+        public string push(int number, int top_index, int max_len)
+        {
+            string strResult = "";
+            if (top_index == max_len)
+            {
+                strResult = "Error in push-stack is full";
+                Console.WriteLine("Error in push-stack is full");
+            }
+            else
+            {
+                int topindex = ++top_index;
+                stack_ref[topindex] = number;
+                strResult = Convert.ToString(topindex);
+                Console.WriteLine(topindex);
+            }
+            return strResult;
+        }
+        public string pop(int top_index)
+        {
+            string strpopResult;
+            if (top_index == -1)
+            {
+                strpopResult = "Error in push-stack is empty";
+                Console.WriteLine("Error in push-stack is empty");
+            }
+            else
+            {
+                int topindex = --top_index;
+                strpopResult = Convert.ToString(topindex);
+
+            }
+            return strpopResult;
+        }
+        public int top()
+        {
+            int top_index = 0;
+            stack_ref[top_index] = 10;
+            return (stack_ref[top_index]);
+        }
+        public bool empty()
+        {
+            int top_index = -1;
+            return (top_index == -1);
+        }
     }
 }
